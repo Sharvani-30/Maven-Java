9d92279245a025975fe2c5a3474e0d8158b56d2e
Sharvani <sharvanikomuravelly2004@gmail.com>
2024-11-20 13:11:43 +0530

    .

---
+++ src/main/java/demo/sample/com/Sample_demo_javaproject/App.java
@@ -0,0 +1,13 @@
+package demo.sample.com.Sample_demo_javaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
